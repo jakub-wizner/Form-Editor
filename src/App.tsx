54a788afd3674280c8191c7eb0df436cf3882d08
Jakub <kubawizner@DESKTOP-QMGCQ3N>
2023-09-21 09:15:12 +0200

    Implement logic behind changing category form and product form, rendering and logging data to the console

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,52 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react';
+import { Layout, Menu } from 'antd';
+import { translations } from './config/translations';
+import { configElements } from './config/configElements';
+import { FieldConfig } from './config/fieldConfig';
+import ElementForm from './components/ElementForm';
 
-function App() {
+const { Content, Sider } = Layout;
+
+const App: React.FC = () => {
+  const [currentElement, setCurrentElement] = React.useState<string | null>(null);
+
+  const handleMenuClick = (element: string) => {
+    setCurrentElement(element);
+  };
+
+  const handleFormSubmit = (data: Record<string, any>) => {
+    console.log('Form data:', data);
+  };
+
+  const renderForm = () => {
+    if (currentElement) {
+      const config: FieldConfig[] = configElements[currentElement];
+      return <ElementForm config={config} onSubmit={handleFormSubmit} />;
+    }
+    return null;
+  };
 
   return (
-    <>
-      <div>
-        Form Editor
-      </div>
-    </>
-  )
-}
+    <Layout style={{ minHeight: '100vh' }}>
+      <Sider width={200} theme="light">
+        <Menu mode="inline" theme="light" onClick={({ key }) => handleMenuClick(key)}>
+          {Object.keys(configElements).map((element) => (
+            <Menu.Item key={element}>{element}</Menu.Item>
+          ))}
+        </Menu>
+      </Sider>
+      <Layout>
+        <Content style={{ padding: '24px' }}>
+          {currentElement && (
+            <div>
+              <h1>{translations.en[currentElement]}</h1>
+              {renderForm()}
+            </div>
+          )}
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
 
-export default App
+export default App;
