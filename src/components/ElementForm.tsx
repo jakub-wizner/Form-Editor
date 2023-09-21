47ee2b7b78103fbcf832f583039842bd862e0e81
Jakub <kubawizner@DESKTOP-QMGCQ3N>
2023-09-21 09:12:22 +0200

    Implement form logic: handle submitting and clearing the form after its submission

--- src/components/ElementForm.tsx
+++ src/components/ElementForm.tsx
@@ -9,5 +9,55 @@ interface ElementFormProps {
   onSubmit: (data: Record<string, any>) => void;
 }
 
+const ElementForm: React.FC<ElementFormProps> = ({ config, onSubmit }) => {
+  const [form] = Form.useForm();
+  const [submitting, setSubmitting] = useState(false);
+  const [available, setAvailable] = useState(false);
 
+  const handleCheckboxChange = (e) => {
+    setAvailable(e.target.checked);
+  }
+
+  const handleSubmit = async () => {
+    try {
+      setSubmitting(true);
+      const values = await form.validateFields();
+      onSubmit({ ...values, 'product.available': available });
+      form.resetFields();
+      setAvailable(false);
+    } catch (error) {
+      console.error('Form validation error:', error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
+  return (
+    <Form form={form} layout="vertical">
+      {config.map((field) => (
+        <Form.Item key={field.translationKey} label={field.label} name={field.translationKey}>
+          {field.type === 'string' && <Input />}
+          {field.type === 'number' && <Input type="number" />}
+          {field.type === 'choice' && (
+            <Select>
+              {field.choices?.map((choice) => (
+                <Option key={choice} value={choice}>
+                  {choice}
+                </Option>
+              ))}
+            </Select>
+          )}
+          {field.type === 'boolean' && <Checkbox name="product.available" checked={available} onChange={handleCheckboxChange} />}
+        </Form.Item>
+      ))}
+      <Form.Item>
+        <Button type="primary" onClick={handleSubmit} loading={submitting}>
+          Save
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default ElementForm;
 
