b97e37e494952ca721a86c7e35ba95630c032849
Jakub <kubawizner@DESKTOP-QMGCQ3N>
2023-09-21 08:55:54 +0200

    Create translations constant that stores english field names

---
+++ src/config/translations.ts
@@ -0,0 +1,10 @@
+export const translations: Record<string, Record<string, string>> = {
+    en: {
+      'product.name': 'Name',
+      'product.size': 'Size',
+      'product.price': 'Price',
+      'product.available': 'Available',
+      'category.name': 'Name',
+      'category.order': 'Order',
+    },
+  };
