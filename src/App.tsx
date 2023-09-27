5883d5496e99f3ea2130809dadb49ea28a7d173c
Jakub Wizner <kuba.wizner@gmail.com>
2023-09-27 13:15:14 +0200

    Create dropdown for selecting language, temporarily just log language to the console

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Layout, Menu } from 'antd';
-import { translations } from './config/translations';
+import { Layout, Menu, Select } from 'antd';
 import { configElements } from './config/configElements';
 import { FieldConfig } from './config/fieldConfig';
 import ElementForm from './components/ElementForm';
 
-const { Content, Sider } = Layout;
+const { Content, Sider, Header } = Layout;
+const { Option } = Select;
 
 const App: React.FC = () => {
   const [currentElement, setCurrentElement] = React.useState<string | null>(null);
@@ -28,6 +28,10 @@ const App: React.FC = () => {
     console.log('Form data:', data);
   };
 
+  const handleLanguageChange = (value: string) => {
+    console.log('Selected language:', value);
+  };
+
   const renderForm = () => {
     if (currentElement) {
       const config: FieldConfig[] = configElements[currentElement];
@@ -39,13 +43,18 @@ const App: React.FC = () => {
   return (
     <Layout style={{ minHeight: '100vh' }}>
       <Sider width={200} theme="light">
-        <Menu mode="inline" theme="light" onClick={handleMenuClickWrapper} items={menuItems}/>
+        <Header style={{ display: 'inline-block', justifyItems: 'center', margin: 0, backgroundColor: 'white', paddingLeft: 0 }}>
+          <Select defaultValue="en" style={{ width: 120 }} onChange={handleLanguageChange}>
+            <Option value="en">English</Option>
+            <Option value="es">Español</Option>
+          </Select>
+        </Header>
+        <Menu mode="inline" theme="light" onClick={handleMenuClickWrapper} items={menuItems} />
       </Sider>
       <Layout>
         <Content style={{ padding: '24px' }}>
           {currentElement && (
             <div>
-              <h1>{translations.en[currentElement]}</h1>
               {renderForm()}
             </div>
           )}
